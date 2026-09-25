e5e7046777127bd7b1a5b6fc11fe94664f8b2bdf
Эльвира Фоменко <fomenko.eg@dvfu.ru>
2026-09-25 14:24:49 +1000

    Убран дублирующий вывод рентабельности

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ def main():
 
     print(df)
     print("Средняя выручка:", df["Выручка"].mean())
-    profitability = calculate_profitability(150000, 100000)
-    print("Рентабельность:", profitability, "%")
 
 profitability = calculate_profitability(150000, 100000)
 print("Рентабельность:", profitability, "%")
